1f1fa7fafdc437835b7770d664662921be8a7f1b
Willem <willem.pion@gmail.com>
2023-11-14 12:11:46 +0100

    couleur aléatoire

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ import myColleaguesNames from "./index.js";
 import chalk from 'chalk';
 
 for (let i = 0; i < myColleaguesNames.length; i++){
-    console.log(chalk.bgAnsi256(i) (myColleaguesNames[i]))
+    let couleur = Math.round(Math.random()*100);
+    console.log(chalk.bgAnsi256(couleur) (myColleaguesNames[i]))
 }
